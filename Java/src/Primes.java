db1858b652622dba13bfe28da5fc755a15c756fa
Christopher Cowell <christopher.w.cowell@gmail.com>
2019-12-21 09:31:26 -0800

    tweak Java

--- Java/src/Primes.java
+++ Java/src/Primes.java
@@ -9,28 +9,26 @@ public class Primes {
         myPrimes.findPrimes(PRIME_COUNT);
     }
 
-    private boolean isPrime(final long num) {
-        final long max_factor = (long) (Math.floor(Math.sqrt(num)));
-        for (long factor = 2; factor <= max_factor; factor++) {
-            if (num % factor == 0) return false;
-        }
-        return true;
-    }
-
-    private void findPrimes(final long PRIME_COUNT) {
+    private void findPrimes(final long primeCount) {
         final long startMillis = System.currentTimeMillis();
-
         List<Long> primes = new ArrayList<>();
         primes.add(2L);
-
         long candidate = 3L;
-        while (primes.size() < PRIME_COUNT) {
+        while (primes.size() < primeCount) {
             if (isPrime(candidate)) primes.add(candidate);
             candidate += 2L;
         }
-
         final long elapsedMillis = System.currentTimeMillis() - startMillis;
+
         System.out.println("last prime: " + primes.get(primes.size() - 1));
         System.out.println("elapsed: " + elapsedMillis / 1000.0 + " seconds");
+    }
+    
+    private boolean isPrime(final long num) {
+        final long max_factor = (long) (Math.floor(Math.sqrt(num)));
+        for (long factor = 2L; factor <= max_factor; factor++) {
+            if (num % factor == 0) return false;
+        }
+        return true;
     }
 }
